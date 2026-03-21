7287fa95ef3798d951d3bde75b95836533193e96
Karl Wette <karl.wette@anu.edu.au>
2026-03-22 00:48:30 +1100

    Add boilerplate

---
+++ src/one_liners/__init__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2026 Karl Wette
+#
+# SPDX-License-Identifier: MIT
+
+"""Various one-liner pre-commit hooks."""
+
+from typing import Sequence
+
+__author__ = "Karl Wette"
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    """Main function."""
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
