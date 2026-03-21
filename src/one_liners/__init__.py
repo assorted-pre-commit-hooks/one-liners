ee4ba3eb96873c302ae3f7704c87a3e1b18905de
Karl Wette <karl.wette@anu.edu.au>
2026-03-22 03:52:12 +1100

    chmod: change file permissions

--- src/one_liners/__init__.py
+++ src/one_liners/__init__.py
@@ -5,6 +5,8 @@
 """Various one-liner pre-commit hooks."""
 
 import argparse
+import stat
+from functools import partial, reduce
 from pathlib import Path
 from typing import Sequence
 
@@ -21,6 +23,51 @@ class one_liner:
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         one_liner.registry[cls.__name__] = cls
+
+
+class chmod(one_liner):
+    """Change file permissions."""
+
+    @staticmethod
+    def addf(m, f):
+        return m | f
+
+    @staticmethod
+    def subf(m, f):
+        return m & ~f
+
+    def __init__(self, parser):
+        for opt, modef, f in (
+            # --- owner ---
+            ("--uR", chmod.addf, stat.S_IRUSR),
+            ("--uW", chmod.addf, stat.S_IWUSR),
+            ("--uX", chmod.addf, stat.S_IXUSR),
+            ("--ur", chmod.subf, stat.S_IRUSR),
+            ("--uw", chmod.subf, stat.S_IWUSR),
+            ("--ux", chmod.subf, stat.S_IXUSR),
+            # --- group ---
+            ("--gR", chmod.addf, stat.S_IRGRP),
+            ("--gW", chmod.addf, stat.S_IWGRP),
+            ("--gX", chmod.addf, stat.S_IXGRP),
+            ("--gr", chmod.subf, stat.S_IRGRP),
+            ("--gw", chmod.subf, stat.S_IWGRP),
+            ("--gx", chmod.subf, stat.S_IXGRP),
+            # --- other ---
+            ("--oR", chmod.addf, stat.S_IROTH),
+            ("--oW", chmod.addf, stat.S_IWOTH),
+            ("--oX", chmod.addf, stat.S_IXOTH),
+            ("--or", chmod.subf, stat.S_IROTH),
+            ("--ow", chmod.subf, stat.S_IWOTH),
+            ("--ox", chmod.subf, stat.S_IXOTH),
+        ):
+            parser.add_argument(
+                opt, dest="perms", action="append_const", const=partial(modef, f=f)
+            )
+
+    def __call__(self, args, filepath):
+        filepath.chmod(
+            reduce(lambda m, modef: modef(m), [filepath.stat().st_mode] + args.perms)
+        )
 
 
 class regex(one_liner):
--- test/test_one_liners.py
+++ test/test_one_liners.py
@@ -4,7 +4,73 @@
 
 """Test one-liners."""
 
+import stat
+
+import pytest
+
 from one_liners import main
+
+
+@pytest.mark.parametrize(
+    "opt, f",
+    [
+        # --- owner ---
+        ("--uR", stat.S_IRUSR),
+        ("--uW", stat.S_IWUSR),
+        ("--uX", stat.S_IXUSR),
+        # --- group ---
+        ("--gR", stat.S_IRGRP),
+        ("--gW", stat.S_IWGRP),
+        ("--gX", stat.S_IXGRP),
+        # --- other ---
+        ("--oR", stat.S_IROTH),
+        ("--oW", stat.S_IWOTH),
+        ("--oX", stat.S_IXOTH),
+    ],
+)
+def test_chmod_addf(tmp_path, opt, f):
+    """Test chmod adding permissions."""
+
+    tmp_file = tmp_path / "chmod.dat"
+    tmp_file.write_text(":")
+
+    tmp_file.chmod(0)
+    assert stat.S_IMODE(tmp_file.stat().st_mode) == 0
+
+    main(["chmod", opt, str(tmp_file)])
+
+    assert stat.S_IMODE(tmp_file.stat().st_mode) == f
+
+
+@pytest.mark.parametrize(
+    "opt, f",
+    [
+        # --- owner ---
+        ("--ur", stat.S_IRUSR),
+        ("--uw", stat.S_IWUSR),
+        ("--ux", stat.S_IXUSR),
+        # --- group ---
+        ("--gr", stat.S_IRGRP),
+        ("--gw", stat.S_IWGRP),
+        ("--gx", stat.S_IXGRP),
+        # --- other ---
+        ("--or", stat.S_IROTH),
+        ("--ow", stat.S_IWOTH),
+        ("--ox", stat.S_IXOTH),
+    ],
+)
+def test_chmod_subf(tmp_path, opt, f):
+    """Test chmod removing permissions."""
+
+    tmp_file = tmp_path / "chmod.dat"
+    tmp_file.write_text(":")
+
+    tmp_file.chmod(0o777)
+    assert stat.S_IMODE(tmp_file.stat().st_mode) == 0o777
+
+    main(["chmod", opt, str(tmp_file)])
+
+    assert stat.S_IMODE(tmp_file.stat().st_mode) == 0o777 & ~f
 
 
 def test_regex(tmp_path):
